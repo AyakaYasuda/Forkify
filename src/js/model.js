dc6d72004b96df458c9842219c122b3dde97e370
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-01-01 01:04:59 -0800

    created page buttons

--- src/js/model.js
+++ src/js/model.js
@@ -1,13 +1,16 @@
 import { async } from 'regenerator-runtime';
 import { API_URL } from './config';
 import { getJSON } from './helpers';
+import { RES_PER_PAGE } from './config';
 
 export const state = {
   recipe: {},
   search: {
     query: '',
     results: [],
-  }
+    resultsPerPage: RES_PER_PAGE,
+    page: 1,
+  },
 };
 
 // https://forkify-api.herokuapp.com/v2
@@ -55,3 +58,11 @@ export const loadSearchResults = async function (query) {
   }
 };
 
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage; // 0;
+  const end = page * state.search.resultsPerPage; // 9;
+  
+  return state.search.results.slice(start, end);
+};
